c1b00a00ff3546f6974481747c45752da1bdc50d
Vic Gurov <gurovvic@gmail.com>
2023-04-20 15:02:44 -0500

    dt 20 apr 2023

--- TDD_tests/003_start_your_test_general_knowledge.py
+++ TDD_tests/003_start_your_test_general_knowledge.py
@@ -14,8 +14,8 @@ PSSWRD_FLD = (By.ID, "password")
 SGN_IN_BTN = (By.XPATH, "//button[@class='w-100 btn btn-lg btn-primary']")
 LOGIN = "gurovvic@gmail.com"
 PSSWRD = "QWERTY_GUROV"
-# GNRL_KNWLDG_TXT_BTN = EC.presence_of_element_located((By.XPATH, "(//div[@class='d-grid gap-2 m-3'])[1]"))
-GNRL_KNWLDG_TXT_BTN = EC.presence_of_element_located((By.XPATH, "//a[text()='General knowledge (continue)']"))
+GNRL_KNWLDG_TXT_BTN = EC.presence_of_element_located((By.XPATH, "(//div[@class='d-grid gap-2 m-3'])[1]"))
+# GNRL_KNWLDG_TXT_BTN = EC.presence_of_element_located((By.XPATH, "//a[text()='General knowledge (continue)']"))
 AIR_BRKS_TXT_BTN = EC.presence_of_element_located((By.XPATH, "(//div[@class='d-grid gap-2 m-3'])[2]"))
 CMBNTNS_VHCL_TXT_BTN = EC.presence_of_element_located((By.XPATH, "(//div[@class='d-grid gap-2 m-3'])[3]"))
 ELDT_HZMT_ENDRSMNT_TXT_BTN = EC.presence_of_element_located((By.XPATH, "(//div[@class='d-grid gap-2 m-3'])[4]"))
@@ -35,10 +35,11 @@ wait.until(EC.presence_of_element_located(PSSWRD_FLD)).send_keys(PSSWRD)
 wait.until(EC.element_to_be_clickable(SGN_IN_BTN)).click()
 
 # 3. Verify "General knowledge (continue)" button text is here
-expected_text = 'General knowledge (continue)'
+expected_text_1 = 'General knowledge'
+expected_text_2 = 'General knowledge (continue)'
 actual_text = wait.until(GNRL_KNWLDG_TXT_BTN).text
-assert expected_text in actual_text
-print(f'Expected username "{expected_text}", and got: "{actual_text}"\n')
+assert expected_text_1 == actual_text or expected_text_2 == actual_text
+print(f'Expected username "{expected_text_1}" or "{expected_text_2}", and got: "{actual_text}"\n')
 
 # 4. Verify "Air brakes" button text is here
 expected_text = 'Air brakes'
